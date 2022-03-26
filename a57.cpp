d30a41d82a055b315ab0af702af644f69880da75
mdalarmansorker <mdalarmansorker@gmail.com>
2022-03-26 15:29:21 +0600

    testing cpp file added

---
+++ a57.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i = 1, j = 1, k = -1;
+    
+   
+   int  m = ++i && ++j && ++k;
+   cout<<m<<endl;
+    cout<<endl<<i<<" "<<j<<" "<<k;
+    return 0;
+}
